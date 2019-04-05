8e6f729c579e542199154090c42114049769dbe4
Adrian Gordon <adriangordon60@gmail.com>
2019-04-05 08:37:59 +0100

    fixed for crontab

--- src/betfairproxy/buildbatch.js
+++ src/betfairproxy/buildbatch.js
@@ -10,14 +10,14 @@ const delay = nconf.get("delay")
 
 const tasks = nconf.get("tasks")
 
-let bashstring="sudo chmod a+rwx task*.sh\n"
+let bashstring="sudo chmod a+rwx " + dir + "/task*.sh\n"
 
 for(let i=0; i< tasks.length; i++){
   const task = tasks[i]
   const marketid = task.marketid
   const tasktime = task.time
 
-  const outstr = "echo \"sudo node " + dir + "/src/betfairproxy/marketts --conf=" + dir + "/testconfig.json --marketid=" + marketid + " --iterations=" + iterations + " --delay=" + delay + "\" > task" + i + ".sh"
+  const outstr = "echo \"sudo node " + dir + "/src/betfairproxy/marketts --conf=" + dir + "/testconfig.json --marketid=" + marketid + " --iterations=" + iterations + " --delay=" + delay + "\" > " + dir + "/task" + i + ".sh"
 
   console.log(outstr)
   bashstring+= "sudo at -f " + dir + "/task" + i + ".sh " + tasktime + "\n"
